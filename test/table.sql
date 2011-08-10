74afd9aacabbd49e07a2365f36f31b37a2348045
Ian Young <iany@amcoonline.net>
2011-08-10 14:40:19 -0700

    Remove SQL dump

--- test/table.sql
+++
@@ -1,6 +0,0 @@
-DROP TABLE IF EXISTS `enum_test_models`;
-CREATE TABLE `enum_test_models` (
-  `id` int(11) NOT NULL DEFAULT '0',
-  `color` enum('blue','red','yellow') DEFAULT NULL,
-  PRIMARY KEY (`id`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8;
