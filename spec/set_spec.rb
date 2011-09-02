20818f73226fb3f086f10bc48473ed5281f8ddea
Ian Young <iany@amcoonline.net>
2011-09-02 16:05:06 -0700

    Test get/set

--- spec/set_spec.rb
+++ spec/set_spec.rb
@@ -42,9 +42,41 @@ describe "SET datatype" do
   end
 
   describe "assignment" do
-    it "accepts single value"
-    it "accepts array of values"
-    it "accepts comma-separated values"
-    it "accepts empty list"
+    before { load_schema "set_new" }
+    it "accepts single value" do
+      b = Balloon.create :gasses => "helium"
+      b.should be_valid
+      b.reload.gasses.should == "helium"
+    end
+    it "accepts array of values" do
+      pending "Not yet implemented"
+      b = Balloon.create :gasses => [ "helium", "hydrogen" ]
+      b.should be_valid
+      b.reload.gasses.should == "helium,hydrogen"
+    end
+    it "accepts comma-separated values" do
+      b = Balloon.create :gasses => "helium,hydrogen"
+      b.should be_valid
+      b.reload.gasses.should == "helium,hydrogen"
+    end
+    it "accepts empty list" do
+      b = Balloon.create :gasses => [ ]
+      b.should be_valid
+      b.reload.gasses.should == ""
+    end
+  end
+
+  describe "getter" do
+    before do
+      load_schema "set_new"
+      ActiveRecord::Base.connection.execute "INSERT INTO balloons (gasses) VALUES ('helium,hydrogen')"
+      @b = Balloon.first
+    end
+    it "returns comma-separated values by default" do
+      @b.gasses.should == "helium,hydrogen"
+    end
+    it "returns array of values when config option is set"
   end
 end
+
+class Balloon < ActiveRecord::Base; end
