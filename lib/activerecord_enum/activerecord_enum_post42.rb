11577c254803e4fb797a48c87b60b854a7cc62ca
Ian Young <ian@iangreenleaf.com>
2015-04-30 11:07:45 -0700

    Only monkey-patch mysql adapter if it exists

--- lib/activerecord_enum/activerecord_enum_post42.rb
+++ lib/activerecord_enum/activerecord_enum_post42.rb
@@ -1,30 +1,32 @@
 module ActiveRecord
   module ConnectionAdapters
-    class AbstractMysqlAdapter
-      protected
-      def initialize_type_map_with_enum(m)
-        initialize_without_enum(m)
-        register_enum_type(m, %r(^enum)i)
-        register_set_type(m, %r(^set)i)
-      end
+    if defined?(AbstractMysqlAdapter)
+      class AbstractMysqlAdapter
+        protected
+        def initialize_type_map_with_enum(m)
+          initialize_without_enum(m)
+          register_enum_type(m, %r(^enum)i)
+          register_set_type(m, %r(^set)i)
+        end
 
-      alias_method :initialize_without_enum, :initialize_type_map
-      alias_method :initialize_type_map, :initialize_type_map_with_enum
+        alias_method :initialize_without_enum, :initialize_type_map
+        alias_method :initialize_type_map, :initialize_type_map_with_enum
 
-      def register_enum_type(mapping, key)
-        mapping.register_type(key) do |sql_type|
-          if sql_type =~ /(?:enum)\(([^)]+)\)/i
-            limit = $1.scan( /'([^']*)'/ ).flatten
-            Type::Enum.new(limit: limit)
+        def register_enum_type(mapping, key)
+          mapping.register_type(key) do |sql_type|
+            if sql_type =~ /(?:enum)\(([^)]+)\)/i
+              limit = $1.scan( /'([^']*)'/ ).flatten
+              Type::Enum.new(limit: limit)
+            end
           end
         end
-      end
 
-      def register_set_type(mapping, key)
-        mapping.register_type(key) do |sql_type|
-          if sql_type =~ /(?:set)\(([^)]+)\)/i
-            limit = $1.scan( /'([^']*)'/ ).flatten
-            Type::Set.new(limit: limit)
+        def register_set_type(mapping, key)
+          mapping.register_type(key) do |sql_type|
+            if sql_type =~ /(?:set)\(([^)]+)\)/i
+              limit = $1.scan( /'([^']*)'/ ).flatten
+              Type::Set.new(limit: limit)
+            end
           end
         end
       end
